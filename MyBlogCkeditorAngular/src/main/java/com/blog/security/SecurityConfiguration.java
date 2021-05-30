0eeee41995ea68f3459ee8fccec999eed729330c
GuitounYoucef <guitxyoucef@gmail.com>
2021-05-31 00:47:27 +0100

    passwordEncoder

--- MyBlogCkeditorAngular/src/main/java/com/blog/security/SecurityConfiguration.java
+++ MyBlogCkeditorAngular/src/main/java/com/blog/security/SecurityConfiguration.java
@@ -1,12 +1,15 @@
 package com.blog.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
@@ -19,8 +22,7 @@ UserDetailsService userDetailsService;
 @Override
 //Authentication
 protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-	// TODO Auto-generated method stub
-	super.configure(auth);
+	auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
 }
 
 // Authorization
@@ -38,12 +40,15 @@ protected void configure(AuthenticationManagerBuilder auth) throws Exception {
    .permitAll()
    .antMatchers("/blog/posts/{id}")
    .permitAll()   
-   .antMatchers("/blog/testblog")
-   .permitAll()   
    .anyRequest()
    .authenticated();
    //.and()
    //.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 	}
+
+@Bean
+PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+}   
  
 }
